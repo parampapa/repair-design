e8d41d7b42f31751834a753d2f59d0a1876026c2
parampapa <alexandrpeks@gmail.com>
2020-03-17 22:35:56 +0300

    feat
    add script j-Query in index.html and rewright js in to j-Query

--- js/main.js
+++ js/main.js
@@ -1,15 +1,13 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-  const modal = document.querySelector(".modal")
-  const modalBtn = document.querySelectorAll("[data-toggle=modal]")
-  const closeBtn = document.querySelector(".modal__close"); 
-  const swithModal = () => {
-    modal.classList.toggle("modal--visible");
-  }
-  modalBtn.forEach(element => {
-    element.addEventListener("click", swithModal);
-});
 
-closeBtn.addEventListener("click", swithModal)
-
-
+$(document).ready(function () {
+  var modal = $(".modal"),
+      modalBtn = $("[data-toggle=modal]"),          
+      closeBtn = $(".modal__close");
+  
+  modalBtn.on("click", function () {
+    modal.toggleClass("modal--visible")
+  });
+  closeBtn.on("click", function () {
+    modal.toggleClass("modal--visible")
+  });    
 });
